75675e1a4b837c9280b8350e8b358cd60f5f4621
arifsptra <arif.sptrra@gmail.com>
2022-09-13 10:13:48 +0700

    learn math operators

---
+++ belajar-java/src/OperatorMatematika.java
@@ -0,0 +1,27 @@
+public class OperatorMatematika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        // Augmented Assignments
+        System.out.println(a+=10);
+        System.out.println(a-=10);
+        System.out.println(a*=10);
+        System.out.println(a/=10);
+        System.out.println(a%=10);
+
+        // Unary Operator
+        // ++ = a=a+1
+        // -- = a=a-1
+        // - = negatif
+        // + = positif
+        // ! = boolean kebalikan
+        System.out.println(!true);
+    }
+}
