57bbe80dafbb7477359343e12f653024fcdf5eed
arifsptra <arif.sptrra@gmail.com>
2022-09-13 10:34:17 +0700

    learn boolean operators

--- belajar-java/src/OperasiBoolean.java
+++ belajar-java/src/OperasiBoolean.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class OperasiBoolean {
+public class OperasiBoolean {
+    public static void main(String[] args) {
+        // operasi boolean
+        // && = and
+        // || = or
+        // ! = not
+        var absen = 74;
+        var nilaiAkhir = 88;
+
+        var lulusAbsen = absen >= 75;
+        var lulusNilai = nilaiAkhir >= 75;
+
+        var lulus = lulusAbsen && lulusNilai;
+        System.out.println(lulus);
+        lulus = lulusAbsen || lulusNilai;
+        System.out.println(lulus);
+    }
 }
