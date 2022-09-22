670bca37ed40e9e94bf14d5b03ed68bcd29615f6
arifsptra <arif.sptrra@gmail.com>
2022-09-22 11:12:48 +0700

    make a simple calculator

--- 01BasicApplication/app/src/main/java/com/example/a01basicapplication/MainActivity.java
+++ 01BasicApplication/app/src/main/java/com/example/a01basicapplication/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.a01basicapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculator(View view) {
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
     }
 }
