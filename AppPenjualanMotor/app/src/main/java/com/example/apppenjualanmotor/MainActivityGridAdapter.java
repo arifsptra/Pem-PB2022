54898ac225d794cd5e01126d5bba1888b73c93d3
arifsptra <arif.sptrra@gmail.com>
2022-10-25 23:02:57 +0700

    make display activity detail

--- AppPenjualanMotor/app/src/main/java/com/example/apppenjualanmotor/MainActivityGridAdapter.java
+++ AppPenjualanMotor/app/src/main/java/com/example/apppenjualanmotor/MainActivityGridAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -37,25 +38,24 @@ public class MainActivityGridAdapter extends RecyclerView.Adapter<MainActivityGr
         holder.ckodemotor.setText(arrayList.get(position).kode_motor);
         holder.chargamotor.setText(arrayList.get(position).harga_motor);
         holder.cgambarmotor.setImageResource(arrayList.get(position).gambar_motor);
-//        holder.listLayout.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                String namaMobil = arrayList.get(position).nama_motor;
-//                String kodeMobil = arrayList.get(position).kode_motor;
-//                String hargaMobil = arrayList.get(position).harga_motor;
-//                int imageMobil = arrayList.get(position).gambar_motor;
-//
-////                Intent intent = new Intent(context, MainActivityDetail.class);
-////
-////                intent.putExtra("nama_mobil", namaMobil);
-////                intent.putExtra("kode_mobil", kodeMobil);
-////                intent.putExtra("satuan_mobil", satuanMobil);
-////                intent.putExtra("harga_mobil", hargaMobil);
-////                intent.putExtra("image_mobil", imageMobil);
-////
-////                context.startActivity(intent);
-//            }
-//        });
+        holder.listLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String namaMotor = arrayList.get(position).nama_motor;
+                String kodeMotor = arrayList.get(position).kode_motor;
+                String hargaMotor = arrayList.get(position).harga_motor;
+                int imageMotor = arrayList.get(position).gambar_motor;
+
+                Intent intent = new Intent(context, MainActivityDetail.class);
+
+                intent.putExtra("nama_motor", namaMotor);
+                intent.putExtra("kode_motor", kodeMotor);
+                intent.putExtra("harga_motor", hargaMotor);
+                intent.putExtra("image_motor", imageMotor);
+
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -64,12 +64,12 @@ public class MainActivityGridAdapter extends RecyclerView.Adapter<MainActivityGr
     }
 
     public static class myViewHolder extends  RecyclerView.ViewHolder {
-//        RelativeLayout listLayout;
+        LinearLayout listLayout;
         TextView cnamamotor, ckodemotor, chargamotor;
         ImageView cgambarmotor;
         public myViewHolder(@NonNull View itemView){
             super(itemView);
-//            listLayout = itemView.findViewById(R.id.grid_layout);
+            listLayout = itemView.findViewById(R.id.grid_layout);
             cnamamotor = itemView.findViewById(R.id.namaMotorText);
             ckodemotor = itemView.findViewById(R.id.kodeMotorText);
             chargamotor = itemView.findViewById(R.id.hargaMotorText);
--- AppPenjualanMotor/app/src/main/java/com/example/apppenjualanmotor/MainActivityListAdapter.java
+++ AppPenjualanMotor/app/src/main/java/com/example/apppenjualanmotor/MainActivityListAdapter.java
@@ -2,10 +2,12 @@ package com.example.apppenjualanmotor;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -35,25 +37,24 @@ public class MainActivityListAdapter extends RecyclerView.Adapter<MainActivityLi
         holder.ckodemotor.setText(arrayList.get(position).kode_motor);
         holder.chargamotor.setText(arrayList.get(position).harga_motor);
         holder.cgambarmotor.setImageResource(arrayList.get(position).gambar_motor);
-//        holder.listLayout.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                String namaMobil = arrayList.get(position).nama_motor;
-//                String kodeMobil = arrayList.get(position).kode_motor;
-//                String hargaMobil = arrayList.get(position).harga_motor;
-//                int imageMobil = arrayList.get(position).gambar_motor;
-//
-////                Intent intent = new Intent(context, MainActivityDetail.class);
-////
-////                intent.putExtra("nama_mobil", namaMobil);
-////                intent.putExtra("kode_mobil", kodeMobil);
-////                intent.putExtra("satuan_mobil", satuanMobil);
-////                intent.putExtra("harga_mobil", hargaMobil);
-////                intent.putExtra("image_mobil", imageMobil);
-////
-////                context.startActivity(intent);
-//            }
-//        });
+        holder.listLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String namaMotor = arrayList.get(position).nama_motor;
+                String kodeMotor = arrayList.get(position).kode_motor;
+                String hargaMotor = arrayList.get(position).harga_motor;
+                int imageMotor = arrayList.get(position).gambar_motor;
+
+                Intent intent = new Intent(context, MainActivityDetail.class);
+
+                intent.putExtra("nama_motor", namaMotor);
+                intent.putExtra("kode_motor", kodeMotor);
+                intent.putExtra("harga_motor", hargaMotor);
+                intent.putExtra("image_motor", imageMotor);
+
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -62,12 +63,12 @@ public class MainActivityListAdapter extends RecyclerView.Adapter<MainActivityLi
     }
 
     public static class myViewHolder extends  RecyclerView.ViewHolder {
-        //        RelativeLayout listLayout;
+        LinearLayout listLayout;
         TextView cnamamotor, ckodemotor, chargamotor;
         ImageView cgambarmotor;
         public myViewHolder(@NonNull View itemView){
             super(itemView);
-//            listLayout = itemView.findViewById(R.id.grid_layout);
+            listLayout = itemView.findViewById(R.id.grid_layout);
             cnamamotor = itemView.findViewById(R.id.namaMotorText);
             ckodemotor = itemView.findViewById(R.id.kodeMotorText);
             chargamotor = itemView.findViewById(R.id.hargaMotorText);
